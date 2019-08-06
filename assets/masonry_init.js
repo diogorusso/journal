edd4608c9c1a9c59603160608282f65c5cebc4c8
diogorusso <hello.diogorusso@gmail.com>
2019-08-05 20:07:29 -0700

    masonry layout

--- assets/masonry_init.js
+++ assets/masonry_init.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     // Filter isotope
     $container.isotope({
         // options
-        isInitLayout: false,
+        // isInitLayout: false,
         itemSelector: ".course",
         percentPosition: true,
         masonry: {
